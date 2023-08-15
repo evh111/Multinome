c4e4216f17e01bd52274a4f545265b81a50db189
Elijah Helmandollar <elijahhelmandollar101@gmail.com>
2023-08-15 12:53:15 -0400

    Initial Commit

---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Elijah Helmandollar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
